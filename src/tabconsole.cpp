cd71adb2e8ae6ee3ff31970f9000c12b63c65c5e
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-08-16 10:04:11 -0700

    Moved TabConsole-specific functions to class

--- src/tabconsole.cpp
+++ src/tabconsole.cpp
@@ -11,6 +11,13 @@ TabConsole::TabConsole(QWidget *parent) :
 	QObject::connect(
 			ui->console, &QPlainTextEdit::customContextMenuRequested,
 			this, &TabConsole::context_menu );
+
+	// Validate line edit's path every time it's updated.
+	// This is important because the line edit's path is the authoritative
+	// source of what executable is being run in the console.
+	QObject::connect(
+			ui->lineEdit_path, &QLineEdit::editingFinished,
+			this, &TabConsole::validate_path );
 }
 
 TabConsole::~TabConsole()
@@ -18,9 +25,58 @@ TabConsole::~TabConsole()
 	delete ui;
 }
 
-void TabConsole::clear_buffer()
+bool TabConsole::is_path_valid()
 {
-	ui->console->setPlainText("");
+	QDir path_check = QDir(get_path());
+	// QDir::exists only checks for files if absolute path is given
+	if (path_check.exists(get_path()) && get_path().endsWith(".exe"))
+		return true;
+	else
+		return false;
+}
+
+QString TabConsole::get_exe_name()
+{
+	QString exe_name = "";
+	if (is_path_valid()) {
+		int i = get_path().lastIndexOf("/");
+		exe_name = get_path().mid(i);	// QString doesn't have substr
+		exe_name.remove(".exe");		// is_path_valid() guarantees this
+	}
+
+	// Since this function is just looking for a textual representation
+	// of the .exe file, if one isn't found, placeholder text makes sense
+	// (instead of returning null).
+	if (exe_name == "") exe_name = "UNKNOWN";
+	return exe_name;
+}
+
+void TabConsole::validate_path()
+{
+	// don't highlight text box if empty
+	if (is_path_valid() || get_path() == "") {
+		// reset stylesheet
+		ui->lineEdit_path->setStyleSheet("");
+	} else {
+		// highlight text
+		ui->lineEdit_path->setStyleSheet(invalid_path_stylesheet);
+	}
+}
+
+void TabConsole::update_buttons(QProcess::ProcessState state)
+{
+	switch (state) {
+	case QProcess::Starting : break;	// don't care about this
+	case QProcess::Running :
+		ui->button_stop->setEnabled(true);
+		ui->button_run->setText("Restart");
+		// Cosmetic change. Slot always attempts to stop process first.
+		break;
+	case QProcess::NotRunning :
+		ui->button_stop->setEnabled(false);
+		ui->button_run->setText("Run");
+		break;
+	}
 }
 
 void TabConsole::context_menu(const QPoint& pos)
--- src/tabconsole.h
+++ src/tabconsole.h
@@ -1,8 +1,11 @@
 #ifndef TABCONSOLE_H
 #define TABCONSOLE_H
 
+#include <QDir>
 #include <QMenu>
 #include <QPlainTextEdit>
+#include <QProcess>
+#include <QString>
 #include <QWidget>
 
 #include "ui_tabconsole.h"
@@ -21,11 +24,26 @@ public:
 	explicit TabConsole(QWidget *parent = nullptr);
 	~TabConsole();
 
+	bool is_path_valid();
+	QString get_path()	{ return ui->lineEdit_path->text(); }
+	QString get_exe_name();
+
 public slots:
-	void clear_buffer();
+	void clear_buffer() { ui->console->setPlainText(""); }
+	void validate_path();
+	void update_buttons(QProcess::ProcessState state);
+
+private:
+	inline static const QString invalid_path_stylesheet =
+			"QLineEdit {"
+			"color: white;"
+			"background-color: orangered;"
+			"}" ;
 
 private slots:
 	void context_menu(const QPoint& pos);
+
+	void on_button_clear_clicked() { clear_buffer(); }
 };
 
 #endif // TABCONSOLE_H
