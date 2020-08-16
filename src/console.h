c0db22da05de97d1f07b99608facbe664d145999
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-08-16 10:23:08 -0700

    Updated Console class code to use TabConsole
    also moved relevant things over into TabConsole

--- src/console.h
+++ src/console.h
@@ -5,16 +5,12 @@
 #include <QDesktopServices>
 #include <QDir>
 #include <QFile>
-#include <QMap>
 #include <QObject>
-#include <QPoint>
 #include <QProcess>
 #include <QRegularExpression>
 #include <QStandardPaths>
 #include <QString>
 #include <QTextStream>
-
-#include <QMenu>
 
 #include <QFileDialog>
 #include <QMainWindow>
@@ -27,53 +23,38 @@ class Console : public QObject
 	Q_OBJECT
 
 public:
-	static const unsigned int lines_buf = 1024;
-	static const unsigned int chars_buf = 80 * lines_buf;
+	inline static const unsigned int lines_buf = 1024;
+	inline static const unsigned int chars_buf = 80 * lines_buf;
 
-	// Cannot be static, or <APPNAME> isn't registered correctly
-	const QString path_appdata =
-			QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) + "/";
-	const QString path_saved_paths	= path_appdata + "paths.txt";
-	const QString path_logs			= path_appdata + "Logs/";
+	// Cannot be static. <APPNAME> isn't registered correctly on static init,
+	// and `QStandardPaths::AppDataLocation` is not known.
+	// TODO: see if declaring as `inline static` will allow this to work?
+	const QString path_logs =
+			QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+			"/Logs/";
 
-	Console(
-			QMainWindow* parent,
-			QProcess* process,
-			QString prefix_data,
-			QString prefix_logs,
-			QString name_exe );
+	Console(QMainWindow* parent, QString path = "");
 	~Console();
 
-signals:
-	void state_changed(QProcess::ProcessState new_state);
-
-public slots:
-	void context_menu(const QPoint& pos);
-
-	void start_process();
-	void stop_process();
-	void set_path();
-	void clear_buffer();
-	void open_logs();
-
 private:
+	inline static const QString runs_separator = "\n\n";
+
 	QMainWindow* parent;
 	TabConsole* host;
-
 	QProcess* process;
 	QTextStream logger;
 
-	QString prefix_data;
-	QString prefix_logs;
-	QString name_exe;
-
-	QMap<QString, QString> load_path_data(QString file);
-
-	QString load_exe_path();
-	void save_exe_path(QString path);
-
 private slots:
+	// console display
 	void pipe_output();
+
+	// executable path
+	void set_path();
+
+	// action buttons
+	void start_process();
+	void stop_process();
+	void open_logs();
 };
 
 #endif // CONSOLE_H
