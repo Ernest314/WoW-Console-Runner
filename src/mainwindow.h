bef3c532ddcd9f6f1397fead4281d218ca49abb3
Ernest Gu <ernest.gu.3.14@gmail.com>
2020-08-16 15:40:47 -0700

    Added tab saving/loading
    closes #56.

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,18 +1,22 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <QFile>
 #include <QFont>
 #include <QHBoxLayout>
 #include <QPalette>
 #include <QProcess>
 #include <QPushButton>
+#include <QStandardPaths>
 #include <QStyleFactory>
 #include <QTabBar>
 #include <QTabWidget>
+#include <QTextStream>
 #include <QWidget>
 
 #include <QMainWindow>
 
+#include <unordered_map>
 #include <vector>
 
 #include "utils.h"
@@ -32,11 +36,19 @@ public:
 	~MainWindow();
 
 private:
+	// `path_saved_paths` also sounds bad...
+	const QString path_paths =
+			QStandardPaths::writableLocation(QStandardPaths::AppDataLocation) +
+			"/paths.txt";
+
 	Ui::MainWindow* ui;
+	std::unordered_map<QWidget*, Console*> consoles = {};
 
 	std::vector<QString> load_saved_paths();
 
 private slots:
+	void update_saved_paths();
+	void add_tab_blank() { add_tab(); }
 	void add_tab(QString path="");
 	void remove_tab(int index);
 };
